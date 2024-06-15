cb737de700a5ad4a38807950bca8d94e54c1968a
hayden <hmfravel@gmail.com>
2024-06-14 22:28:25 -0400

    Secret Sanata Generator is working. Add more testing next

---
+++ src/src/hashed/HashedPeoplePair.java
@@ -0,0 +1,32 @@
+package hashed;
+
+import crude.Person;
+
+public class HashedPeoplePair
+{
+    private final HashedPerson giver;
+    private final HashedPerson receiver;
+
+    public HashedPeoplePair(HashedPerson giver, HashedPerson receiver)
+    {
+        this.giver = giver;
+        this.receiver = receiver;
+        System.out.println("Creating pair: " + this);
+    }
+
+    public HashedPerson getGiver()
+    {
+        return giver;
+    }
+
+    public HashedPerson getReceiver()
+    {
+        return receiver;
+    }
+
+    public String toString()
+    {
+        return "{'" + giver.getName() + "' -> '" + receiver.getName() + "'}";
+    }
+
+}
--- src/src/hashed/HashedSecretSantaGenerator.java
+++ src/src/hashed/HashedSecretSantaGenerator.java
@@ -1,16 +1,21 @@
 package hashed;
 
+import crude.PeoplePair;
 import util.FailedGenerationException;
 
 import java.util.HashMap;
+import java.util.Random;
 import java.util.Scanner;
 
 public class HashedSecretSantaGenerator
 {
     private static final char DELIMITER = ' ';
     private static final char STOP = 'n';
+    private static final Random RANDOM = new Random();
 
     private HashMap<String, HashedPerson> people;
+    private HashMap<String, HashedPerson> completedPeople;
+    private HashedPeoplePair[] secretSantaPairing;
 
     public void start()
     {
@@ -22,22 +27,43 @@ public class HashedSecretSantaGenerator
             {
                 System.out.println("Type your people, separated by a single space: ");
                 people = createPersonMapFromString(scanner.nextLine());
+                completedPeople = new HashMap<>();
                 initializeEachPersonsValidReceivers();
 
-                System.out.println(this);
                 promptToRemoveInvalidReceivers(scanner);
+                System.out.println(this);
+
+                secretSantaPairing = new HashedPeoplePair[people.size()];
+
+                int pairingCount = 0;
+                while (pairingCount < secretSantaPairing.length)
+                {
+                    HashedPerson[] matchingGroup = findMatchingGroups();
+                    if (matchingGroup != null)
+                    {
+                        System.out.println("Matching Group: " + matchingGroup.length);
+                        randomlyCreatePairs(matchingGroup);
+                        pairingCount += matchingGroup.length;
+                    }
+                    else
+                    {
+                        System.out.println("Single Person");
+                        pairFirstPerson();
+                        pairingCount++;
+                    }
+                }
+
+                System.out.println(peoplePairToString(secretSantaPairing));
             }
             catch (FailedGenerationException e)
             {
                 System.out.println("Ran into the following error, try again:");
                 e.printStackTrace();
             }
-            finally
-            {
-                System.out.print("Do you want to go again (y or n)? ");
-                if (scanner.nextLine().charAt(0) == STOP)
-                    run = false;
-            }
+
+            System.out.print("Do you want to go again (y or n)? ");
+            if (scanner.nextLine().charAt(0) == STOP)
+                run = false;
         }
     }
 
@@ -114,6 +140,96 @@ public class HashedSecretSantaGenerator
         }
     }
 
+    private HashedPerson[] findMatchingGroups() throws FailedGenerationException
+    {
+        HashedPerson[] peopleWithTheSameReceivers;
+        int peopleCount;
+
+        for (HashedPerson person1 : people.values())
+        {
+            peopleCount = 1;
+            peopleWithTheSameReceivers = new HashedPerson[person1.getValidReceivers().size()];
+
+            for (HashedPerson person2 : people.values())
+            {
+                if (person1 != person2 && person2.getValidReceivers().equals(person1.getValidReceivers()))
+                {
+                    if (peopleCount == person1.getValidReceivers().size())
+                        throw new FailedGenerationException("Cannot create Secret Santa, too few connections");
+
+                    peopleWithTheSameReceivers[peopleCount] = person2;
+                    peopleCount++;
+                }
+            }
+
+            if (peopleCount == person1.getValidReceivers().size())
+            {
+                peopleWithTheSameReceivers[0] = person1;
+                return peopleWithTheSameReceivers;
+            }
+        }
+
+        return null;
+    }
+
+    private void randomlyCreatePairs(HashedPerson[] givingPeople) throws FailedGenerationException
+    {
+        HashMap<String, HashedPerson> validReceivers = givingPeople[0].getValidReceivers();
+        int count = givingPeople.length;
+
+        if (count != validReceivers.size())
+            throw new FailedGenerationException("Amount of Givers (" + count + ") vs Receivers ("
+                    + validReceivers.size() + ") does not match");
+
+        for (HashedPerson receiver : validReceivers.values())
+        {
+            int randomIndex = RANDOM.nextInt(count);
+            HashedPerson giver = givingPeople[randomIndex];
+            secretSantaPairing[completedPeople.size()] = new HashedPeoplePair(giver, receiver);
+
+            completedPeople.put(giver.getName(), people.remove(giver.getName()));
+            removePersonFromAllReceivingGroups(receiver.getName());
+
+            count--;
+            givingPeople[randomIndex] = givingPeople[count];
+        }
+    }
+
+    private void pairFirstPerson() throws FailedGenerationException
+    {
+        String name = people.keySet().iterator().next();
+        HashedPerson giver = people.remove(name);
+
+        HashedPerson receiver = chooseRandomPerson(giver.getValidReceivers());
+        if (receiver == null)
+            throw new FailedGenerationException("Unable to find a receiver for " + name);
+        removePersonFromAllReceivingGroups(receiver.getName());
+
+        secretSantaPairing[completedPeople.size()] = new HashedPeoplePair(giver, receiver);
+        completedPeople.put(name, giver);
+    }
+
+    private HashedPerson chooseRandomPerson(HashMap<String, HashedPerson> hashedPeople)
+    {
+        int randomIndex = RANDOM.nextInt(hashedPeople.size());
+        for (HashedPerson person : hashedPeople.values())
+        {
+            if (randomIndex == 0)
+                return person;
+            randomIndex--;
+        }
+
+        return null;
+    }
+
+    private void removePersonFromAllReceivingGroups(String name)
+    {
+        for (HashedPerson person : people.values())
+        {
+            person.removeReceiver(name);
+        }
+    }
+
     public String toString()
     {
         StringBuilder output = new StringBuilder();
@@ -130,5 +246,16 @@ public class HashedSecretSantaGenerator
     {
         HashedSecretSantaGenerator secretSantaGenerator = new HashedSecretSantaGenerator();
         secretSantaGenerator.start();
+    }
+
+    private static String peoplePairToString(HashedPeoplePair[] peoplePairs)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (HashedPeoplePair peoplePair : peoplePairs)
+        {
+            stringBuilder.append(peoplePair).append("\n");
+        }
+
+        return stringBuilder.toString();
     }
 }
