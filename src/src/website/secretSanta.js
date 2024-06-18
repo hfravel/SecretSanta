422963d60485e41b3fc2adb4412e955dfa19e1fe
hayden <hmfravel@gmail.com>
2024-06-18 19:31:20 -0400

    All setup code is done. Improved readability of the code

--- src/src/website/secretSanta.js
+++ src/src/website/secretSanta.js
@@ -1,20 +1,24 @@
-function PersonObject(name)
+class PersonObject 
 {
-    this.name = name;
-    this.connections = new Map();
-    this.restrictions = new Map();
+    constructor(name) 
+    {
+        this.name = name;
+        this.connections = new Map();
+        this.restrictions = new Map();
+    }
 }
 
 const PEOPLE_BUTTON_LIST = "peopleList";
 const PERSON_NAME = "personName";
 const CONNECTIONS_TABLE = "connectionsTable";
 const RESTRICTIONS_TABLE = "restrictionsTable";
+const NO_PERSON = "No People Created";
 
 const peopleMap = new Map();
 
 function addNewPerson()
 {
-    let peopleList = document.getElementById(PEOPLE_BUTTON_LIST);
+    let peopleButtonList = document.getElementById(PEOPLE_BUTTON_LIST);
     let newPerson = new PersonObject(window.prompt("Enter name:", ""));
     for (const [personName, person] of peopleMap.entries())
     {
@@ -24,14 +28,73 @@ function addNewPerson()
 
     peopleMap.set(newPerson.name, newPerson);
 
-    let newButton = document.createElement("button");
-    newButton.id = newPerson.name;
-    newButton.classList.add("button");
-    newButton.textContent = newPerson.name;
-    newButton.addEventListener("click", openPersonPage);
+    let newPersonButton = document.createElement("button");
+    newPersonButton.id = newPerson.name;
+    newPersonButton.classList.add("button");
+    newPersonButton.textContent = newPerson.name;
+    newPersonButton.addEventListener("click", openPage);
 
-    peopleList.appendChild(newButton);
-    openPage(newPerson);
+    peopleButtonList.appendChild(newPersonButton);
+    openPersonPage(newPerson);
+}
+
+function openPage(event)
+{
+    let openPerson = peopleMap.get(event.target.innerHTML);
+    openPersonPage(openPerson);
+}
+
+function openPersonPage(openPerson)
+{
+    clearPage(openPerson.name);
+
+    let connectionsTable = document.getElementById(CONNECTIONS_TABLE);
+    for (const person of openPerson.connections.values())
+    {
+        addRowToTable(connectionsTable, person.name);
+    }
+
+    let restrictionsTable = document.getElementById(RESTRICTIONS_TABLE);
+    for (const person of openPerson.restrictions.values())
+    {
+        addRowToTable(restrictionsTable, person.name);
+    }
+}
+
+function clearPage(personName)
+{
+    document.getElementById(PERSON_NAME).innerHTML = personName;
+    removeTableRows(CONNECTIONS_TABLE);
+    removeTableRows(RESTRICTIONS_TABLE);
+}
+
+function removeTableRows(tableName)
+{
+    let table = document.getElementById(tableName);
+    let rowCount = table.rows.length;
+    
+    for (let i = 1; i < rowCount; i++) 
+    {
+        table.deleteRow(1);
+    }
+}
+
+function addRowToTable(table, personName)
+{
+    let row = table.insertRow(-1);
+    row.insertCell(0).innerHTML = personName;
+    let removeCell = row.insertCell(1);
+
+    let movePersonButton = document.createElement("button");
+    movePersonButton.classList.add("button");
+    movePersonButton.textContent = "Move";
+
+    if (table.id == RESTRICTIONS_TABLE)
+        movePersonButton.addEventListener("click", moveToConnections);
+    else
+        movePersonButton.addEventListener("click", moveToRestrictions);
+
+    removeCell.appendChild(movePersonButton);
 }
 
 function addPersonToAll(newPerson)
@@ -44,25 +107,19 @@ function addPersonToAll(newPerson)
 
 function removePerson()
 {
-    let personName = document.getElementById(PERSON_NAME).innerHTML;
-    peopleMap.delete(personName);
-    removePersonFromAll(personName);
+    let removalPersonName = document.getElementById(PERSON_NAME).innerHTML;
+    if (peopleMap.delete(removalPersonName))
+    {
+        removePersonFromAll(removalPersonName);
 
-    let personButton = document.getElementById(personName);
-    personButton.parentNode.removeChild(personButton);
+        let removalPersonButton = document.getElementById(removalPersonName);
+        removalPersonButton.parentNode.removeChild(removalPersonButton);
 
-    if (peopleMap.size == 0)
-        emptyPage();
-    else
-        openPage(peopleMap.values().next().value);
-}
-
-function emptyPage()
-{
-    document.getElementById(PERSON_NAME).innerHTML = "No Person";
-    removeTableRows(CONNECTIONS_TABLE);
-    removeTableRows(RESTRICTIONS_TABLE);
-
+        if (peopleMap.size == 0)
+            clearPage(NO_PERSON);
+        else
+            openPersonPage(peopleMap.values().next().value);
+    }
 }
 
 function removePersonFromAll(removalPersonName)
@@ -71,58 +128,6 @@ function removePersonFromAll(removalPersonName)
     {
         person.connections.delete(removalPersonName);
         person.restrictions.delete(removalPersonName);
-    }
-}
-
-function openPersonPage(event)
-{
-    let openPerson = peopleMap.get(event.target.innerHTML);
-    openPage(openPerson);
-}
-
-function openPage(openPerson)
-{
-    document.getElementById(PERSON_NAME).innerHTML = openPerson.name;
-    removeTableRows(CONNECTIONS_TABLE);
-    removeTableRows(RESTRICTIONS_TABLE);
-
-    let connectionsTable = document.getElementById(CONNECTIONS_TABLE);
-    for (const person of openPerson.connections.values())
-    {
-        addRowToTable(connectionsTable, person.name, false);
-    }
-
-    let restrictionsTable = document.getElementById(RESTRICTIONS_TABLE);
-    for (const person of openPerson.restrictions.values())
-    {
-        addRowToTable(restrictionsTable, person.name, true);
-    }
-}
-
-function addRowToTable(table, personName, isRestricted)
-{
-    let row = table.insertRow(-1);
-    row.insertCell(0).innerHTML = personName;
-    let removeCell = row.insertCell(1);
-
-    let newButton = document.createElement("button");
-    newButton.classList.add("button");
-    newButton.textContent = "remove";
-    if (isRestricted)
-        newButton.addEventListener("click", moveToConnections);
-    else
-        newButton.addEventListener("click", moveToRestrictions);
-
-    removeCell.appendChild(newButton);
-}
-
-function removeTableRows(tableName)
-{
-    let tableHeaderRowCount = 1;
-    let table = document.getElementById(tableName);
-    let rowCount = table.rows.length;
-    for (let i = tableHeaderRowCount; i < rowCount; i++) {
-        table.deleteRow(tableHeaderRowCount);
     }
 }
 
@@ -136,13 +141,30 @@ function moveToRestrictions(event)
 
     let movingPerson = peopleMap.get(connectionsTable.rows[tableRow].cells[0].innerHTML);
     connectionsTable.deleteRow(tableRow);
-    addRowToTable(restrictionsTable, movingPerson.name, true);
+    addRowToTable(restrictionsTable, movingPerson.name);
 
     mainPerson.connections.delete(movingPerson.name);
     mainPerson.restrictions.set(movingPerson.name, movingPerson);
 }
 
-function moveToConnections()
+function moveToConnections(event)
+{
+    let mainPerson = peopleMap.get(document.getElementById(PERSON_NAME).innerHTML);
+    let tableRow = event.target.parentNode.parentNode.rowIndex;
+
+    let connectionsTable = document.getElementById(CONNECTIONS_TABLE);
+    let restrictionsTable = document.getElementById(RESTRICTIONS_TABLE);
+
+    let movingPerson = peopleMap.get(restrictionsTable.rows[tableRow].cells[0].innerHTML);
+    restrictionsTable.deleteRow(tableRow);
+    addRowToTable(connectionsTable, movingPerson.name);
+
+    mainPerson.restrictions.delete(movingPerson.name);
+    mainPerson.connections.set(movingPerson.name, movingPerson);
+}
+
+// Start of the Secret Santa Generator Code
+function generateSecretSantaPairings()
 {
 
 }
