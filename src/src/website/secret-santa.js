0441c6848ff380cd77ba9a04ebc1d10d6a9767b3
hayden <hmfravel@gmail.com>
2024-06-18 20:00:47 -0400

    Updated ReadMe and created framework for Secret Santa Generator

---
+++ src/src/website/secret-santa.js
@@ -0,0 +1,35 @@
+
+function generateSecretSantaPairings()
+{
+    console.log(peopleMap);
+}
+
+// No arguments, just use peopleMap to find groups of people with same connections
+function findMatchingGroups()
+{
+
+}
+
+// list of people to give, use their matching connections object
+function randomlyCreatePairs()
+{
+
+}
+
+// no arguments, just use peopleMap to get the first Person
+function pairFirstPerson()
+{
+
+}
+
+// connections HashMap, choose one at random to assign to mainPerson
+function chooseRandomPerson()
+{
+
+}
+
+// string name, remove this person from all connections
+function removePersonFromAllReceivingGroups()
+{
+
+}
